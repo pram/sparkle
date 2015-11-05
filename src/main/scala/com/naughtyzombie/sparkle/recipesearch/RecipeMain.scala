5c67fbb4c562314115ec47e3f88670eb9c15c257
pram <pram@attale.com>
2015-11-05 19:35:29 +0000

    adding reader

--- src/main/scala/com/naughtyzombie/sparkle/recipesearch/RecipeMain.scala
+++ src/main/scala/com/naughtyzombie/sparkle/recipesearch/RecipeMain.scala
@@ -1,5 +1,6 @@
 package com.naughtyzombie.sparkle.recipesearch
 
+import com.naughtyzombie.sparkle.recipesearch.model.Recipe
 import org.apache.spark._
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
@@ -14,8 +15,30 @@ object RecipeMain {
   def logger = LoggerFactory.getLogger("RecipeMain")
   def main(args: Array[String]): Unit = {
     if (args.length < 3) {
-      logger.error(s"Usage: RecipeMain inputfile outputfile")
+      logger.error(s"Usage: RecipeMain sparkmaster inputfile outputfile")
       exit(1)
     }
+
+    val master = args(0)
+    val inputFile = args(1)
+    val outputFile = args(2)
+
+    val sc = new SparkContext(master, "RecipeMain",System.getenv("SPARK_HOME"))
+
+    val input = sc.textFile(inputFile)
+
+    val result = input.mapPartitions(records => {
+      val mapper = new ObjectMapper with ScalaObjectMapper
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+      mapper.registerModule(DefaultScalaModule)
+
+      records.flatMap(record => {
+        try {
+          Some(mapper.readValue(record, classOf[Recipe]))
+        } catch {
+          case e: Exception => None
+        }
+      })
+    }, true)
   }
 }
