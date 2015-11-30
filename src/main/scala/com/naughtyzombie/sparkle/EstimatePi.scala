1e50e60047b6b7f1e3802610d41a4e7aea5b28eb
pram <pram@attale.com>
2015-11-30 17:41:45 +0000

    working locally

--- src/main/scala/com/naughtyzombie/sparkle/EstimatePi.scala
+++ src/main/scala/com/naughtyzombie/sparkle/EstimatePi.scala
@@ -1,6 +1,6 @@
 package com.naughtyzombie.sparkle
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by pram on 29/11/2015.
@@ -8,14 +8,18 @@ import org.apache.spark.SparkContext
 object EstimatePi {
   def main(args: Array[String]): Unit = {
 
-    val sc = new SparkContext()
+    val NUM_SAMPLES = 10000
+    val conf = new SparkConf().setAppName("Spark Pi")
+    val sc = new SparkContext("local", "Spark Pi", conf)
 
-    val count = sc.parallelize(1 to 10).map { i =>
+    val count = sc.parallelize(1 to NUM_SAMPLES).map { i =>
       val x = Math.random()
       val y = Math.random()
 
       if (x * x + y * y < 1) 1 else 0
-    }.saveAsTextFile("hdfs://quickstart.cloudera:9000/user/pram/pi-out")
+    }.reduce(_ + _)
+
+    println(s"The value of pi is roughly ${ 4.0 * count / NUM_SAMPLES}")
 
 //    val x = sc.textFile("hdfs://quickstart.cloudera:9000/user/pram/pi-out")
 
