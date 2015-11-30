44b3499afab499ceddd874b9da2757a29a78ce04
pram <pram@attale.com>
2015-11-30 18:36:01 +0000

    checking something

---
+++ src/main/scala/com/naughtyzombie/sparkle/aas/ch2/Test.scala
@@ -0,0 +1,17 @@
+package com.naughtyzombie.sparkle.aas.ch2
+
+/**
+  * Created by pram on 30/11/2015.
+  */
+object Test {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  def isHeader(line: String): Boolean = {
+    line.contains("something")
+  }
+
+
+
+}
