db8277eedfed700e9d88c36114a48bfb1e9d73f9
pram <pram@attale.com>
2015-12-01 19:50:31 +0000

    moving on

--- src/main/scala/com/naughtyzombie/sparkle/aas/ch3/Recommender.scala
+++ src/main/scala/com/naughtyzombie/sparkle/aas/ch3/Recommender.scala
@@ -4,6 +4,8 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.slf4j.LoggerFactory
 
+import scala.collection.Map
+
 /**
   * Created by pram on 01/12/2015.
   */
@@ -12,7 +14,7 @@ object Recommender {
   val logger = LoggerFactory.getLogger("Recommender")
 
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local","Recommender", new SparkConf().setAppName("Recommender"))
+    val sc = new SparkContext("local", "Recommender", new SparkConf().setAppName("Recommender"))
 
     val base = "files_x/audio/"
 
@@ -30,6 +32,38 @@ object Recommender {
     val itemIDStats = rawUserArtistData.map(_.split(' ')(1).toDouble).stats()
     logger.info(userIDStats.toString())
     logger.info(itemIDStats.toString())
+
+    val artistByID = buildArtistByID(rawArtistData)
+    val artistAlias = buildArtistAlias(rawArtistAlias)
+
+    val (badID, goodID) = artistAlias.head
+    logger.info(artistByID.lookup(badID) + " -> " + artistByID.lookup(goodID))
   }
+
+  def buildArtistByID(rawArtistData: RDD[String]) =
+    rawArtistData.flatMap { line =>
+      val (id, name) = line.span(_ != '\t')
+      if (name.isEmpty) {
+        None
+      } else {
+        try {
+          Some((id.toInt, name.trim))
+        } catch {
+          case e: NumberFormatException => None
+        }
+      }
+    }
+
+  def buildArtistAlias(rawArtistAlias: RDD[String]): Map[Int, Int] = {
+    rawArtistAlias.flatMap { line =>
+      val tokens = line.split('\t')
+      if (tokens(0).isEmpty) {
+        None
+      } else {
+        Some((tokens(0).toInt, tokens(1).toInt))
+      }
+    }.collectAsMap()
+  }
+
 
 }
