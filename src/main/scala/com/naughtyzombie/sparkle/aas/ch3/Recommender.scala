44902e2e51400c246a9f2a94c8c20fba776b50f5
pram <pram@attale.com>
2015-12-02 20:00:53 +0000

    creating a model

--- src/main/scala/com/naughtyzombie/sparkle/aas/ch3/Recommender.scala
+++ src/main/scala/com/naughtyzombie/sparkle/aas/ch3/Recommender.scala
@@ -1,5 +1,7 @@
 package com.naughtyzombie.sparkle.aas.ch3
 
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.mllib.recommendation._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.slf4j.LoggerFactory
@@ -18,13 +20,35 @@ object Recommender {
 
     val base = "files_x/audio/"
 
-    val rawUserArtistData = sc.textFile(base + "user_artist_data.txt")
+    val rawUserArtistData = sc.textFile(base + "user_artist_data_small.txt")
     val rawArtistData = sc.textFile(base + "artist_data.txt")
     val rawArtistAlias = sc.textFile(base + "artist_alias.txt")
 
     prepareData(rawUserArtistData, rawArtistData, rawArtistAlias)
+    modelData(sc,rawUserArtistData, rawArtistData, rawArtistAlias)
 
     sc.stop
+  }
+
+  def modelData(sc: SparkContext, rawUserArtistData: RDD[String], rawArtistData: RDD[String], rawArtistAlias: RDD[String]) = {
+    val bArtistAlias = sc.broadcast(buildArtistAlias(rawArtistAlias))
+
+    val trainData = buildRatings(rawUserArtistData, bArtistAlias).cache()
+
+    val model = ALS.trainImplicit(trainData, 10, 5, 0.01, 1.0)
+
+    trainData.unpersist()
+
+    logger.info(model.userFeatures.mapValues(_.mkString(", ")).first().toString())
+
+  }
+
+  def buildRatings(rawUserArtistData: RDD[String], bArtistAlias: Broadcast[Map[Int,Int]]) = {
+    rawUserArtistData.map { line =>
+      val Array(userID, artistID, count) = line.split(' ').map(_.toInt)
+      val finalArtistID = bArtistAlias.value.getOrElse(artistID, artistID)
+      Rating(userID, finalArtistID, count)
+    }
   }
 
   def prepareData(rawUserArtistData: RDD[String], rawArtistData: RDD[String], rawArtistAlias: RDD[String]) = {
