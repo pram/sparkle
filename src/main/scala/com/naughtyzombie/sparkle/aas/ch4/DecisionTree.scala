c8dce8eb3fe1e318b4e0f185749fa3f74df63035
pram <pram@attale.com>
2015-12-07 18:43:06 +0000

    initial setup

--- src/main/scala/com/naughtyzombie/sparkle/aas/ch4/DecisionTree.scala
+++ src/main/scala/com/naughtyzombie/sparkle/aas/ch4/DecisionTree.scala
@@ -1,10 +1,19 @@
 package com.naughtyzombie.sparkle.aas.ch4
 
+import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+
 /**
   * Created by pram on 07/12/2015.
   */
 object DecisionTree {
+
+  val logger = LoggerFactory.getLogger("DecisionTree")
+
   def main(args: Array[String]): Unit = {
+
+    val sc = new SparkContext("local", "DecisionTree", new SparkConf().setAppName("DecisionTree"))
+    val base = "files_x/covtype/"
 
   }
 }
